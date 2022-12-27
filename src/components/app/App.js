4962348ac06c3c9cfe8f58e73f72a5f70574de25
kirill_fedorenko <kurull.fedorenko@gmail.com>
2022-12-27 12:59:16 +0200

    change pages links

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -21,7 +21,7 @@ const App = () => {
 				<main>
 					<Suspense fallback={<Spinner />}>
 						<Routes>
-							<Route path="/" element={<MainPage />} />
+							<Route path="/marvel_app" element={<MainPage />} />
 							<Route path="/comics/*" element={<ComicsPage />} />
 							<Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic' />} />
 							<Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character' />} />
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -5,7 +5,7 @@ const AppHeader = () => {
 	return (
 		<header className="app__header">
 			<h1 className="app__title">
-				<Link to="/">
+				<Link to="/marvel_app">
 					<span>Marvel</span> information portal
 				</Link>
 			</h1>
@@ -14,7 +14,7 @@ const AppHeader = () => {
 					<li><NavLink
 						end
 						style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit' })}
-						to="/">Characters</NavLink></li>
+						to="/marvel_app">Characters</NavLink></li>
 					/
 					<li><NavLink
 						end
